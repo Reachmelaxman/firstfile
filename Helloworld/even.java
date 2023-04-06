cc048eda5ca327f805c9873d38514d21bcecaea0
reachmelaxman@gmail.com <reachmelaxman@gmail.com>
2023-04-06 14:35:32 +0530

    even number

---
+++ Helloworld/even.java
@@ -0,0 +1,11 @@
+package Helloworld;
+
+public class even {
+    public static void main(String[] args) {
+        
+        int n = 12;
+        if(n % 2 == 0){
+            System.out.print("Even");
+        }
+    }
+}
